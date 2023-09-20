c892bfebade5c5d4d6ed407373a382de19707ae2
Callie Giles <anchoredinthecrown@gmail.com>
2023-09-20 11:36:01 -0400

    update fibonacci function

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -3,7 +3,15 @@
  * @param {integer} n
  */
 function fibonacci(n) {
-
+    if (n < 0) {
+        throw Error("Input should be >= 0");
+    } else if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
 module.exports = fibonacci;
